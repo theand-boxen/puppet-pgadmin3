3d30dc3850e6b9df95faa61d65a7eed3a4dc7c77
Heesang Chae <theand@gmail.com>
2015-12-05 12:14:53 +0900

    use 1.20.1

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class pgadmin3 {
   package { 'pgAdmin3':
     provider => 'appdmg_eula',
-    source   => 'http://ftp.postgresql.org/pub/pgadmin3/release/v1.18.1/osx/pgadmin3-1.18.1.dmg',
+    source   => 'https://ftp.postgresql.org/pub/pgadmin3/release/v1.20.0/osx/pgadmin3-1.20.0.dmg',
   }
 }
