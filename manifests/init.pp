6018c542ab8b190accd048474e05c62a25f6d3ac
Heesang Chae <theand@gmail.com>
2016-05-25 20:13:20 +0900

    v1.22.1

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,6 +6,6 @@
 class pgadmin3 {
   package { 'pgAdmin3':
     provider => 'appdmg_eula',
-    source   => 'https://ftp.postgresql.org/pub/pgadmin3/release/v1.20.0/osx/pgadmin3-1.20.0.dmg',
+    source   => 'https://ftp.postgresql.org/pub/pgadmin3/release/v1.22.1/osx/pgadmin3-1.22.1.dmg',
   }
 }
